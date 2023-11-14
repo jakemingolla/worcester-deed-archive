dd0acbfc3f8bd6af1b24b177c40717529ad2f934
Jake Mingolla <jake.mingolla@robinpowered.com>
2023-11-14 10:33:41 -0500

    Added screenshot manager, started integration tests (#3)
    * Added screenshot manager, started integration tests
    
    * added screenshots directory
    
    * make screenshot creation a part of postinstall hook

---
+++ src/core/screenshot.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import { Page } from "puppeteer";
+
+import { log } from "./log";
+
+export class Screenshot {
+  // The counter is used to increment the screenshot filenames.
+  // It is incremented every time a screenshot is recorded.
+  private count: number;
+  constructor() {
+    this.count = 0;
+  }
+
+  /**
+   * Records a screenshot of the current state of the given page.
+   * The screenshot will be saved in the configured screenshot path,
+   * using a combination of the given name and an incremented counter.
+   *
+   * @param page The Puppeteer page to use.
+   * @param name The name to use for the screenshot.
+   */
+  async record(page: Page, name: string): Promise<void> {
+    const filename = String(this.count).padStart(2, "0") + "-" + name + ".png";
+    log.info(`Recording current state in screenshot ${filename}`);
+    await page.screenshot({
+      path: path.join("./screenshots", filename),
+      type: "png",
+    });
+    this.count += 1;
+  }
+}
